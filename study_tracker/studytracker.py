dba82f57d06284caded66c93e55d14d6355bc262
Kit F <cfrancisjr15@gmail.com>
2026-09-01 05:12:06 -0400

    Update studytracker.py

--- study_tracker/studytracker.py
+++ study_tracker/studytracker.py
@@ -1,1 +1,8 @@
+print("===Study Tracker===")
+print("1. Security Fundamentals")
+print("2. Web Programming")
+print("3. Cisco Network Accademy stuff")
 
+choice = input("choose a course:")
+
+print("You Selected:",choice)
